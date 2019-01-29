49375b838a8c5a4511e5f9fb35952537f746a0b4
ipyrogova <iana.pyrogova@generalassemb.ly>
2019-01-29 21:13:00 +0800

    added post item UI. Added Detailed Activity to view the posts

---
+++ app/src/main/java/com/inc/iana/aboutiana/activities/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.inc.iana.aboutiana.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.inc.iana.aboutiana.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
