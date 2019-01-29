b83d21dd8252fccb72ee3602b7a677ff033faf76
ipyrogova <iana.pyrogova@generalassemb.ly>
2019-01-29 21:30:08 +0800

    added json entries classes. Added retrofit into gradel build

---
+++ app/src/main/java/com/inc/iana/aboutiana/jsonEntry/Blog.java
@@ -0,0 +1,20 @@
+package com.inc.iana.aboutiana.jsonEntry;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Blog {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
